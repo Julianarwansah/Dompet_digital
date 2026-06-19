b62f8d89874f17f09b20a21c87d20a69f81e86d0
Julian Arwansah <julianarwansahh@gmail.com>
2026-06-19 09:31:03 +0700

    menambahkan api configuration ke backend

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -3,7 +3,10 @@ class AppConstants {
   static const String appVersion = '1.0.0';
 
   // API
-  static const String baseUrl = 'http://192.168.0.7:8080'; // Android emulator → localhost
+  static const String baseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'http://localhost:8080',
+  );
   static const String apiVersion = '/v1';
   static const int connectTimeout = 30;
   static const int receiveTimeout = 30;
